7c3336ebde40d4f888ede3cdc0c412a3c9817a76
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2018-10-18 17:58:57 +0200

    change format and correct bug with duplicate states

--- src/pyperplan/search/full_state_space_search.py
+++ src/pyperplan/search/full_state_space_search.py
@@ -20,7 +20,7 @@ Implements a complete search until a given limit of expansions.
 '''
 
 from collections import deque
-import logging
+import logging, sys
 
 from . import searchspace
 
@@ -30,6 +30,10 @@ class StateSpaceInfo:
     expanded states.
     Similar to the searchspace.py classes, but used only
     for our purposes.
+
+    NOTE: the data structure used during the search is different from the one
+    printed and used later, we might be able to make it the same
+
     """
 
     def __init__(self):
@@ -42,26 +46,52 @@ class StateSpaceInfo:
     @param state: state from the SearchNode class
     @param parent: parent from the SearchNode class
     """
-    def add_state(self, state, parent):
+    def add_state(self, state, parent, is_goal):
         # corner case for initial state
         if parent is None:
-            self.states[state] = (self.global_id, [])
+            self.states[state] = (self.global_id, [], is_goal)
             self.global_id += 1
             return
         if state in list(self.states.keys()):
-            state_id, parents_list = self.states[state]
+            state_id, parents_list, is_goal = self.states[state]
             parents_list.append(self.get_id(parent.state))
-            self.states[state] = state_id, parents_list
+            self.states[state] = (state_id, parents_list, is_goal)
         else:
-            self.states[state] = (self.global_id, [self.get_id(parent.state)])
+            self.states[state] = (self.global_id, [self.get_id(parent.state)], is_goal)
             self.global_id += 1
 
     def get_id(self, state):
-        state_id, _ = self.states[state]
+        state_id, _, _ = self.states[state]
         return state_id
 
     def _debug_print(self):
-        print (self.states)
+        for i in self.states:
+            print (i, self.states[i])
+
+    def parse_atom(self, atom):
+        atom = atom.replace('(', '')
+        atom = atom.replace(')', '')
+        split = atom.split()
+        action = split[0]
+        new_atom = action+"(" + (",".join(split[1:])) + ")"
+        return new_atom
+
+
+    def convert_and_output(self):
+        output = []
+        for state in self.states:
+            state_id, parents, is_goal = self.states[state]
+            atoms = [self.parse_atom(atom) for atom in state]
+            for parent in parents:
+                new_entry = dict()
+                new_entry["id"] = state_id
+                new_entry["parent"] = parent
+                new_entry["goal"] = is_goal
+                new_entry["atoms"] = atoms
+                output.append(new_entry)
+
+        for i in output:
+            print (i)
 
 def full_state_space_search(planning_task, max_exp=10000):
 
@@ -75,38 +105,43 @@ def full_state_space_search(planning_task, max_exp=10000):
     queue = deque()
     queue.append(searchspace.make_root_node(planning_task.initial_state))
     # set storing the explored nodes, used for duplicate detection
-    closed = {planning_task.initial_state}
+
+    closed = set()
     while queue:
         iteration += 1
         logging.debug("breadth_first_search: Iteration %d, #unexplored=%d"
                       % (iteration, len(queue)))
         # get the next node to explore
         node = queue.popleft()
+        is_goal = planning_task.goal_reached(node.state)
 
+        # add state to our StateSpaceInfo or update its parent
+        state_space.add_state(node.state, node.parent, is_goal)
 
-        # add state to our StateSpaceInfo
-        state_space.add_state(node.state, node.parent)
+        # we manage the closed list here to allow the parents update
+        if node.state in closed:
+            # if it has already been expanded, skip
+            continue
+        else:
+            closed.add(node.state)
 
         # exploring the node or if it is a goal node extracting the plan
-        if planning_task.goal_reached(node.state):
+        if is_goal:
             goals += 1
             logging.info("Goal found. Number of goal states found: %d" % goals)
         if iteration >= max_exp:
             logging.info("Maximum number of expansions reached. Exiting the search.")
             logging.info("Total number of goal states: %d" % goals)
             logging.info("%d Nodes expanded" % iteration)
-            state_space._debug_print()
+            state_space.convert_and_output()
             return None
-        for operator, successor_state in planning_task.get_successor_states(
-                                                                   node.state):
-            # duplicate detection
-            if successor_state not in closed:
-                queue.append(searchspace.make_child_node(node, operator,
-                                                         successor_state))
-                 # remember the successor state
-                closed.add(successor_state)
+        for operator, successor_state in planning_task.get_successor_states(node.state):
+            queue.append(searchspace.make_child_node(node, operator,
+                                                     successor_state))
     logging.info("No operators left.")
     logging.info("%d Nodes expanded" % iteration)
     logging.info("Total number of goal states: %d" % goals)
-    state_space._debug_print()
+
+    state_space.convert_and_output()
+
     return None
