015d3dbcd1be4bccd42246a6ee7a0b675a79e95f
Guillem Frances <guillem.frances@gmail.com>
2018-11-27 16:17:14 +0100

    Move relevant code to SLTP

--- src/basilisk/tester.py
+++ src/basilisk/tester.py
@@ -1,29 +1,11 @@
 #!/usr/bin/env python3
-import logging
 import sys
 
-import sltp
-from sltp.extensions import UniverseIndex, ExtensionCache
-from sltp.features import parse_pddl, try_number
-from sltp.features import Model as FeatureModel
-from tarski.dl import UniversalConcept, EmptyConcept
-from tarski.syntax.transform.errors import TransformationError
-from tarski.syntax.transform.simplifications import transform_to_ground_atoms
+from sltp.features import create_denotation_processor
 from tarski import fstrips
 
 from . import PYPERPLAN_DIR
 from .search import create_pyperplan_hill_climbing_with_embedded_heuristic
-
-
-def compute_goal_denotation(problem, use_goal_denotation):
-    if not use_goal_denotation:
-        return []
-
-    try:
-        return transform_to_ground_atoms(problem.goal)
-    except TransformationError:
-        logging.error("Cannot create goal concepts when problem goal is not a conjunction of ground atoms")
-        raise
 
 
 def state_as_atoms(state):
@@ -54,11 +36,10 @@ def run_pyperplan(pyperplan, domain, instance, heuristic_parameters, use_goal_de
     # we don't have to worry about default parameters. We use "gbf" but will override that next.
     args = pyperplan.parse_args([domain, instance])
 
-    # Parse the domain & instance with Tarski
-    problem, language, generic_constants, types = parse_pddl(domain, instance)
+    # Parse the domain & instance and create a model generator
+    problem, processor = create_denotation_processor(domain, instance, use_goal_denotation)
+
     static_atoms = compute_static_atoms(problem)
-    goal_denotation = compute_goal_denotation(problem, use_goal_denotation)
-    processor = DenotationProcessor(language, goal_denotation)
     pyerplan_heuristic = create_heuristic(processor, static_atoms, heuristic_parameters)
 
     # And now we inject our desired search and heuristic functions
@@ -81,8 +62,7 @@ def create_heuristic(processor, static_atoms, heuristic_parameters):
 
         translated = translate_state(state)
         translated_with_statics = translated + list(static_atoms)
-        cache = processor.create_cache_for_state(translated_with_statics)
-        model = FeatureModel(cache)
+        model = processor.compute_model_from_state(translated_with_statics)
 
         h = 0
         for feature, weight in heuristic_parameters:
@@ -105,32 +85,6 @@ def translate_atom(atom):
 def translate_state(state):
     """ Translate a pyperplan-like state into a list with the format required by SLTP's concept denotation processor """
     return [translate_atom(atom) for atom in state]
-
-
-class DenotationProcessor(object):
-    def __init__(self, language, goal_denotation):
-        self.language = language
-        self.universe_sort = language.get_sort('object')
-        self.top = UniversalConcept(self.universe_sort.name)
-        self.bot = EmptyConcept(self.universe_sort.name)
-        self.universe = compute_universe_from_pddl_model(language)
-        self.processor = sltp.SemanticProcessor(language, [], self.universe, self.top, self.bot)
-        self.goal_denotation = goal_denotation
-
-    def create_cache_for_state(self, state):
-        cache = ExtensionCache(self.universe, self.top, self.bot)
-        # We give an arbitrary state ID to 0 to the only state we're interested in
-        self.processor.register_state_on_cache(0, state, cache, self.goal_denotation)
-        return cache
-
-
-def compute_universe_from_pddl_model(language):
-    """ Compute a Universe Index from the PDDL model """
-    universe = UniverseIndex()
-    for obj in language.constants():
-        universe.add(try_number(obj.symbol))
-    universe.finish()  # No more objects possible
-    return universe
 
 
 def run(config, data, rng):
