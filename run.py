69a04b7ef2ae05ac77b9adb01b242f5a8ff27feb
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2020-02-21 11:34:47 +0100

    Create numpy arrays with input and output values and refactor
       - Split main loop into more functions
       - Add flag to keep intermediate values
       - Add more logging
       - Create numpy arrays for input values (features) and for
         output values (h-star)

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 import argparse
 import logging
 import math
+import numpy as np
 import os
 import shutil
 import subprocess
@@ -27,6 +28,9 @@ def parse_arguments():
                         help='File where h-star distances are output.')
     parser.add_argument('--features', default='features.dat',
                         help='Feature values by state.')
+    parser.add_argument('--keep-files', action='store_true',
+                        help='Keep intermediate files (e.g., h-star table). '
+                             'Experiment files are always store under "runs/".')
 
     args = parser.parse_args()
     return args
@@ -72,6 +76,70 @@ def compute_h_star(exp_dir):
     return dist
 
 
+def define_nn_input_and_output(features_file, h_star):
+    """
+    Organize table of features and h-star values into data structures for Keras
+    """
+    features = defaultdict(list)
+    with open(features_file, 'r') as f:
+        for line in f:
+            values = line.split()
+            features[int(values[0])] = [int(x) for x in values[1:]]
+
+    num_features = len(features[0]) - 1
+    num_states = len(features)
+
+    # Loop over all states adding the features and the h_star value in the
+    # same order, discarding the state ID of them
+    clean_features = []
+    clean_h_star = []
+    for s, f in features.items():
+        clean_features.append(f)
+        clean_h_star.append(h_star[s])
+    return np.array(clean_features), np.array(clean_h_star)
+
+
+def call_experiment(arguments):
+    """
+    Run experiment from the arguments passed in the command line.
+    """
+    logging.info('Running script "{}"'.format(domain_file))
+    logging.info('Running configuration "{}"'.format(arguments.configuration))
+    logging.warning(
+        "Experiment configuration should NOT be an incremental experiment.")
+    with open(arguments.output, 'w') as f:
+        logging.info('Running experiment. Output saved to file "{}".'.format(
+            arguments.output))
+        subprocess.call([domain_file, arguments.configuration,
+                         '1', '2', '3', '4'],
+                        stdout=f)
+        logging.info('Experiment finished.')
+
+
+def get_experiment_directory(arguments):
+    """
+    Obtain experiment directory where all data files are stored.
+    """
+    directory = None
+    with open(arguments.output, 'r') as f:
+        for line in f:
+            if "Using experiment directory" in line:
+                directory = line.replace("Using experiment directory ",
+                                       '').replace('\n', '')
+                logging.info('Experiment directory: "{}"'.format(directory))
+                break
+    return directory
+
+
+def write_feature_and_h_star_files(values, arguments):
+    logging.info('Writing distances to "%s"' % arguments.distances)
+    with open(arguments.distances, 'w') as df:
+        for node, value in values.items():
+            df.write('{} {}\n'.format(node, value))
+    logging.info('Writing features to "%s"' % arguments.features)
+    shutil.copyfile(exp_dir + '/feature-matrix.dat', arguments.features)
+
+
 if __name__ == '__main__':
     args = parse_arguments()
     logging.basicConfig(stream=sys.stdout,
@@ -84,36 +152,18 @@ if __name__ == '__main__':
             'Error: Script file "%s" does not exist.\n' % domain_file)
         sys.exit()
 
-    logging.info('Running script "{}"'.format(domain_file))
-    logging.info('Running configuration "{}"'.format(args.configuration))
-    logging.warning(
-        "Experiment configuration should NOT be an incremental experiment.")
-
-    with open(args.output, 'w') as f:
-        logging.info('Running experiment. Output saved to file "{}".'.format(
-            args.output))
-        subprocess.call([domain_file, args.configuration, '1', '2', '3', '4'],
-                        stdout=f)
-        logging.info('Experiment finished.')
-
-    exp_dir = None
-    with open(args.output, 'r') as f:
-        for line in f:
-            if "Using experiment directory" in line:
-                exp_dir = line.replace("Using experiment directory ",
-                                       '').replace('\n', '')
-                logging.info('Experiment directory: "{}"'.format(exp_dir))
-                break
+    call_experiment(args)
+    exp_dir = get_experiment_directory(args)
 
     if exp_dir is None:
         logging.error('No experiment directory found!')
         sys.exit()
 
     h_star = compute_h_star(exp_dir)
+    if args.keep_files:
+        write_feature_and_h_star_files(h_star, args)
 
-    logging.info('Writing distances to "%s"' % args.distances)
-    with open(args.distances, 'w') as df:
-        for node, value in h_star.items():
-            df.write('{} {}\n'.format(node, value))
-    logging.info('Writing features to "%s"' % args.features)
-    shutil.copyfile(exp_dir+'/feature-matrix.dat', args.features)
+    # Set up data for keras
+    input_features, output_values = define_nn_input_and_output(args.features,
+                                                               h_star)
+    logging.info("Setting up input features and output values for NN.")
